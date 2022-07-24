c3dfbd3b8ab16e3154348fd06c6e381480de5e45
Mega <theidealist254>
2022-07-24 15:37:23 +0300

    sec

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,20 @@ app.use('/api/products', require('./routes/productsRoutes'))
 app.use('/api/orders', require('./routes/orderRoutes'))
 app.use('/api/carts', require('./routes/cartRoutes'))
 app.use('/api/v1/admins', require('./routes/adminRoutes'))
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      )
+    );
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+  }
+
 app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
